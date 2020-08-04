54943063fe3f6c379c9033446b4f3f21585328ac
elyabe <elyabe.as@gmail.com>
2020-08-04 19:54:16 -0300

    Added migrations and database connection

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,12 +2,10 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    const users = [
-        { name: 'Elyabe', age: 51},
-        { name: 'Elyabe', age: 51},
-    ]
-    return response.json(users);
+app.use(express.json());
+
+app.get('/', (request, response) => {
+    return response.json({ message: 'Hello World'});
 })
 
 
