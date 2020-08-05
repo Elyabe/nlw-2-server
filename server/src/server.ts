d8fd58610ca13e1bbec4558f727da4a6db0f03a7
elyabe <elyabe.as@gmail.com>
2020-08-04 21:14:42 -0300

    Add cors, controllers and routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,12 @@
 import express from 'express';
+import routes from './routes';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
-
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello World'});
-})
-
+app.use(cors());
+app.use(routes);
 
 app.listen(3333);
 
